ae19e2a6a52a0b77d7b921bb1511b048fe011541
ousmane91 <contact@ousmangueye.fr>
2019-04-09 11:38:31 +0200

    exo fini

---
+++ script.js
@@ -0,0 +1,12 @@
+//fonction pour attribué la couleur du bloc au texte
+function colorGreen() {
+  document.getElementById('text').style.color ='green';
+}
+
+function colorRed() {
+  document.getElementById('text').style.color ='red';
+}
+
+function colorBlue() {
+  document.getElementById('text').style.color ='blue';
+}
